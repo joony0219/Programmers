7d85e3be7532f175142d255db6fce70c692b5981
joony0219 <70154828+joony0219@users.noreply.github.com>
2023-07-04 21:29:49 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -4,14 +4,14 @@ function solution(keyinput, board) {
     const maxHeight = (board[1] - 1) / 2;
     
     for(move of keyinput) {
-        if (move === 'left' && -(maxWidth) <= answer[0] - 1) {
-            answer[0] = answer[0] - 1
-        } else if (move === 'right' && maxWidth >= answer[0] + 1) {
-            answer[0] = answer[0] + 1
-        } else if (move === 'up' && maxHeight >= answer[1] + 1) {
-            answer[1] = answer[1] + 1
-        } else if (move === 'down' && -(maxHeight) <= answer[1] - 1) {
-            answer[1] = answer[1] - 1
+        if (move === 'left' && -(maxWidth) < answer[0]) {
+            answer[0] -= 1
+        } else if (move === 'right' && maxWidth > answer[0]) {
+            answer[0] += 1
+        } else if (move === 'up' && maxHeight > answer[1]) {
+            answer[1] += 1
+        } else if (move === 'down' && -(maxHeight) < answer[1]) {
+            answer[1] -= 1
         }
     }
     
