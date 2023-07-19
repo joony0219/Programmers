932aba3b122b2421a95fe1a48451b43df57522be
joony0219 <70154828+joony0219@users.noreply.github.com>
2023-07-19 21:21:07 +0900

    [unrated] Title: 추억 점수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,13 +1,11 @@
 function solution(name, yearning, photo) {
     var answer = [];
     for(let i = 0; i < photo.length; i++) {
-        let row = [];
+        let score = 0;
         for(let j = 0; j < photo[i].length; j++) {
-            if(name.indexOf(photo[i][j]) !== -1) {
-                row.push(yearning[name.indexOf(photo[i][j])])
-            }
+            if(name.indexOf(photo[i][j]) !== -1) score += yearning[name.indexOf(photo[i][j])]
         }
-        answer.push(row.reduce((a,b) => a + b, 0))
+        answer.push(score)
     }
     
     return answer;
